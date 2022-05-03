c58670bab440b3aa3fa5aecb2562bb19240103f2
rinal22 <rinal2295@gmail.com>
2022-05-03 15:41:01 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/my/pkg/MyFirstProgram.java
+++ sandbox/src/main/java/my/pkg/MyFirstProgram.java
@@ -2,7 +2,25 @@ package my.pkg;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
+    hello("Rinal");
 
-    System.out.println("Hello,world!");
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+  }
+
+  public static double area(double l){
+    return l*l;
+  }
+
+  public static double area(double a, double b){
+    return a*b;
   }
 }
