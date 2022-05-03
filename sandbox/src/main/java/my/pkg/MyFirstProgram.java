bc853b4c791be8574f0e93f5c1f8aa40e4ee1728
rinal22 <rinal2295@gmail.com>
2022-05-03 14:43:55 +0300

    Изменение настройки форматирования

--- sandbox/src/main/java/my/pkg/MyFirstProgram.java
+++ sandbox/src/main/java/my/pkg/MyFirstProgram.java
@@ -1,8 +1,8 @@
 package my.pkg;
 
 public class MyFirstProgram {
+  public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        System.out.println("Hello,world!");
-    }
+    System.out.println("Hello,world!");
+  }
 }
