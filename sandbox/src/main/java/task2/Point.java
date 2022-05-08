c74f5f3c976755683c9f195c9764676c6a18a8f2
rinal22 <rinal2295@gmail.com>
2022-05-08 23:28:00 +0300

    add methods distance(Point p1) and printDistance (Point p1)

--- sandbox/src/main/java/task2/Point.java
+++ sandbox/src/main/java/task2/Point.java
@@ -7,7 +7,7 @@ public class Point {
   public Point(double x, double y) {
     this.x = x;
     this.y = y;
-  };
+  }
 
   public static double distance(Point p1, Point p2) {
     return Math.sqrt((p1.x-p2.x) * (p1.x-p2.x) + (p1.y-p2.y) * (p1.y-p2.y));
@@ -16,4 +16,13 @@ public class Point {
   public static void printDistance (Point p1, Point p2) {
     System.out.println("Расстояние между точками x1("+p1.x+","+p1.y+") и x2("+p2.x+","+p2.y+") = " + p2.distance(p1,p2));
   }
+
+  public double distance(Point p1) {
+    return Math.sqrt((p1.x-this.x) * (p1.x-this.x) + (p1.y-this.y) * (p1.y-this.y));
+  }
+
+  public void printDistance (Point p1) {
+    System.out.println("Расстояние между точками x1("+p1.x+","+p1.y+") и x2("+this.x+","+this.y+") = " + this.distance(p1));
+  }
+
 }
--- sandbox/src/main/java/task2/Task2.java
+++ sandbox/src/main/java/task2/Task2.java
@@ -5,11 +5,13 @@ public class Task2 {
     Point p1 = new Point(0,0);
     Point p2 = new Point(4,3);
     p1.printDistance(p1,p2);
+    p1.printDistance(p2);
 
     Point p3 = new Point(4,3);
     Point p4 = new Point(8,6);
     p3.printDistance(p3,p4);
 
+    p3.printDistance(p4);
   }
 
 }
