3c93b91c09cca330bc70299aea6089bedcebd476
rinal22 <rinal2295@gmail.com>
2022-05-15 17:02:28 +0300

    2.8 Выделены отдельные классы, методы разделены

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupHelper extends HelperBase {
+
+  public GroupHelper(WebDriver wd) {
+    super(wd);
+  }
+
+  public void returnToGroupPage() {
+    click(By.linkText("group page"));
+  }
+
+  public void submitGroupCreation() {
+    click(By.name("submit"));
+  }
+
+  public void fillGroupForm(GroupData groupData) {
+    type(By.name("group_name"), groupData.name());
+    type(By.name("group_header"), groupData.header());
+    type(By.name("group_footer"), groupData.footer());
+  }
+
+  public void initGroupCreation() {
+    click(By.name("new"));
+  }
+
+  public void deleteSelectedGroup() {
+    click(By.name("delete"));
+  }
+
+  public void selectGroup() {
+    click(By.xpath("//div[@id='content']/form/span[1]/input"));
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.model;
 
 public record ContactData(String lastname, String address, String home_tel, String firstname, String email) {
 }
