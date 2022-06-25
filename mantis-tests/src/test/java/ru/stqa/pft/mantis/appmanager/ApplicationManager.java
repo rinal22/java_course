62a30cae5a26be71d6ee2ff25d39e52dba21ef67
rinal22 <rinal2295@gmail.com>
2022-06-25 18:14:52 +0300

    8.3 (+ заменила местонахождение файлов)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -26,6 +27,7 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
+
     if (browser. equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)) {
@@ -40,6 +42,18 @@ public class ApplicationManager {
 
   public void stop() {
     wd.quit();
+  }
+
+  public void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  public  HttpSession newSession(){
+    return new HttpSession(this);
+  }
+
+  public Object getProperty(String key) {
+    return properties.getProperty(key);
   }
 
 }
