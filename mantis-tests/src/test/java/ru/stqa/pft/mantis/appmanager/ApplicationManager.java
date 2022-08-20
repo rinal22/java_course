5488925925014b3e9cf4717a0e45a724a52b81fb
rinal22 <rinal2295@gmail.com>
2022-08-20 21:59:53 +0300

    return to 8.2

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,30 +1,21 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   private final Properties properties;
-  private WebDriver wd;
+  public WebDriver wd;
   private String browser;
-  private RegistrationHelper registrationHelper;
-  private FtpHelper ftp;
-  private MailHelper mailHelper;
-  private JamesHelper jamesHelper;
-  private SessionHelper session;
-  private NavigationHelper goTo;
-  private DbHelper dbHelper;
-  private UsersHelper user;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -34,94 +25,21 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-    dbHelper = new DbHelper();
+
+    if (browser. equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+
+    wd.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
   }
 
   public void stop() {
-
-    if (wd != null) {
-      wd.quit();
-    }
+    wd.quit();
   }
 
-  public void logout() {
-    wd.findElement(By.linkText("Logout")).click();
-  }
-
-  public HttpSession newSession() {
-    return new HttpSession(this);
-  }
-
-  public Object getProperty(String key) {
-    return properties.getProperty(key);
-  }
-
-  public RegistrationHelper registration() {
-    if (registrationHelper == null) {
-      registrationHelper = new RegistrationHelper(this);
-    }
-    return registrationHelper;
-  }
-
-  public FtpHelper ftp() {
-    if (ftp == null) {
-      ftp = new FtpHelper(this);
-    }
-    return ftp;
-  }
-
-  public WebDriver getDriver() {
-    if (wd == null) {
-      if (Objects.equals(browser, BrowserType.FIREFOX)) {
-        wd = new FirefoxDriver();
-      } else if (browser.equals(BrowserType.CHROME)) {
-        wd = new ChromeDriver();
-      } else wd = new ChromeDriver();
-
-      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-      wd.get(properties.getProperty("web.baseUrl"));
-    }
-    return wd;
-  }
-
-  public MailHelper mail() {
-    if (mailHelper == null) {
-      mailHelper = new MailHelper(this);
-    }
-    return mailHelper;
-  }
-
-  public JamesHelper james() {
-    if (jamesHelper == null) {
-      jamesHelper = new JamesHelper(this);
-    }
-    return jamesHelper;
-  }
-
-  public SessionHelper session() {
-    if (session == null) {
-      session = new SessionHelper(this);
-    }
-    return session;
-  }
-
-  public NavigationHelper goTo() {
-    if (goTo == null) {
-      goTo = new NavigationHelper(this);
-    }
-    return goTo;
-  }
-
-
-  public UsersHelper user() {
-    if (user == null) {
-      user = new UsersHelper(this);
-    }
-    return user;
-  }
-
-  public DbHelper db() {
-    return dbHelper;
-  }
 }
-
