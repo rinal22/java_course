f4723ff12ef6685a929f1202fc4160a1bf2a2835
rinal22 <rinal2295@gmail.com>
2022-08-21 16:15:33 +0300

    9.2 все еще не работает, замена на java-8 (Zulu)

--- soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -5,11 +5,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GeoIpServiceTests {
-
   @Test
   public void testMyIp() {
     String geoIp = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("46.138.135.251");
     Assert.assertEquals(geoIp, "<GeoIP><Country>RU</Country><State>48</State></GeoIP>");
-    }
+  }
   }
 
